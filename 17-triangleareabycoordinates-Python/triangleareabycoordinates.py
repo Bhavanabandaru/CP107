251efc6a1560a9e6bffefba2bf57fd9e761ae2c4
bhavana <bhavanabandaru19@msitprogram.net>
2021-07-20 11:52:43 +0530

    done

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -6,4 +6,13 @@
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	s1 = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+	s2 = ((x2 - x3)**2 + (y2 - y3)**2)**0.5
+	s3 = ((x3 - x1)**2 + (y3 - y1)**2)**0.5
+
+	p = (s1 + s2 + s3)/2
+	area = (p * (p - s1) * (p - s2) * (p - s3))**0.5
+
+	return area
+
+	
