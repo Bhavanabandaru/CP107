be0f13ac3e2585694b8f0ebaf1240768dd49d77b
bhavana <bhavanabandaru19@msitprogram.net>
2021-07-29 01:11:08 +0530

    done with program

--- isPalindromicprime.py
+++ isPalindromicprime.py
@@ -11,3 +11,36 @@
 # assert (isPalindromicPrime(121) == False)
 # print("All test cases passed... :-)")
 # Note: checkout from the master branch from your visual studio code by trying to search for the problem statement (Search for it). Once you find out, start solving it.
+def isPrime(n):
+	if n<2:
+		return False
+	for factor in range(2,n):
+		if n%factor ==0:
+			return False
+	return True
+def isPalindrome(n):
+	rev =0
+	while (n>0):
+		dig = n%10
+		rev = rev*10+dig
+		n = n//10
+	return rev
+def isPalindromicPrime(n):
+
+    if isPrime(n) and isPalindrome(n):
+        return True
+    
+    return False
+
+
+assert (isPalindromicPrime(2) == True)
+assert (isPalindromicPrime(10) == False)
+assert (isPalindromicPrime(104) == False)
+assert (isPalindromicPrime(235) == False)
+assert (isPalindromicPrime(131) == True)
+assert (isPalindromicPrime(900) == False)
+assert (isPalindromicPrime(101) == True)
+assert (isPalindromicPrime(383) == True)
+assert (isPalindromicPrime(373) == True)
+assert (isPalindromicPrime(121) == False)
+print("All test cases passed... :-)")
