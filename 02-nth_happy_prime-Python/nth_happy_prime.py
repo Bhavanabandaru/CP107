b6e931a5d8a2f82477ce8585eff203015f1266c7
bhavana <bhavanabandaru19@msitprogram.net>
2021-07-28 21:54:01 +0530

    done

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -5,4 +5,42 @@
 
 
 def fun_nth_happy_prime(n):
-	return 0
+	n = abs(n)
+	count = -1
+	i = 2
+	while True:
+		if is_prime(n) and is_happy(n):
+			count += 1
+		if count == n:
+			break
+		i += 1
+
+	return i
+
+def is_happy(n):
+	while(1):
+		if n == 1:
+			return True
+		if n == 4:
+			return False
+		else:
+			sqod = sum_sqof_digits(n)
+
+	return False
+
+def sum_sqof_digits(n):
+	sum = 0
+	while n > 0:
+		sum += (n % 10)**2
+		n = n // 10
+	return sum
+
+def is_prime(n):
+	if n < 2:
+		return False
+	for i in range(2, n):
+		if n % i == 0:
+			return False
+	return True
+
+print(fun_nth_happy_prime(0))	
