ad0a11cf8d0baaf9b426af8a17b740ccd0545988
bhavana <bhavanabandaru19@msitprogram.net>
2021-08-05 00:43:40 +0530

    done

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -6,5 +6,23 @@
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
 
+def isTidy(n):
+    a = n
+    x = 0
+    y = 0
+    while a > 0:
+        x = a % 10
+        a = a //10
+        y = a % 10
+        if x < y:
+            return False
+    return n
+
 def fun_nth_tidynumber(n):
-    return 0
+    count =0
+    x = 1
+    while (count <= n):
+        if (isTidy(x)):
+            count = count + 1
+        x += 1
+    return x-1
