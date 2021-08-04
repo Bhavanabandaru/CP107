d6a67b03e30a50d52527c88c9fbdcd09a7788712
bhavana <bhavanabandaru19@msitprogram.net>
2021-08-04 22:45:24 +0530

    done

--- 04-getaverage-Python/getaverage.py
+++ 04-getaverage-Python/getaverage.py
@@ -10,5 +10,17 @@
 
 
 def fun_getaverage(s): 
-	return 0.0
+	li=list(s.split(","))
+	
+	sum=0
+	cnt=0
+	for i in li:
+		print(i)
+		if i.isdigit():
+			print(i)
+			sum+=int(i)
+			cnt+=1
+	if cnt==0:
+		return 0.0
+	return sum/cnt
 
