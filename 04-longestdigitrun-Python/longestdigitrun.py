ad8b09e33b6e75f13cc2bedbd88594c67b773cb7
bhavana <bhavanabandaru19@msitprogram.net>
2021-08-06 10:50:18 +0530

    done

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -6,4 +6,31 @@
 # as does longestDigitRun(-677886).
 def longestdigitrun(n):
 	# Your code goes here
-	pass
+	x = abs(n)
+	ldRun = 1
+	ld = x % 10
+	a = 1
+	ind = x
+	cont = 0
+	while(ind != 0):
+		ind = ind // 10
+		cont = cont + 1
+	if(cont == 1):
+		return 1
+	
+	for i in range(0,cont):
+		l = x % 10
+		x = x // 10
+		m = x % 10
+		if(l == m):
+			a = a + 1
+		elif(a > ldRun):
+			ldRun = a
+			ld = l
+			a = 1
+		elif(a == ldRun and a < ld):
+			ld = l
+			a = 1
+		else:
+			a = 1
+	return ld
